85ababebce3a1dd60b7d9ac51bd9b81394e2b1fa
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-11-15 21:02:52 +0200

    2

--- src/by/javatr/task2/runner/Main.java
+++ src/by/javatr/task2/runner/Main.java
@@ -1,8 +1,11 @@
 package task2.runner;
+
+import task2.logic.Logic2;
+
 //2. Составить программу, которая по заданным году и номеру месяца определяет количество дней в этом
 //месяце и корректно определялись все високосные года.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("2");
+        System.out.println(Logic2.countOfDaysThisYear());
     }
 }
---
+++ src/by/javatr/task2/util/Data2.java
@@ -0,0 +1,17 @@
+package task2.util;
+
+import scanner.TaskScanner;
+
+public class Data2 {
+    public static int getYear() {
+        int year = TaskScanner.scannerInt();
+        return year;
+    }
+
+    public static int getMonth() {
+        int month = TaskScanner.scannerInt();
+        return month;
+    }
+
+}
+
