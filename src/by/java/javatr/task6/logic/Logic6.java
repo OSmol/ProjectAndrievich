e459b039629b8b864e2435326ebe8aa052337ca0
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-11-18 21:53:54 +0200

    task6

--- src/by/java/javatr/task6/logic/Logic6.java
+++ src/by/java/javatr/task6/logic/Logic6.java
@@ -4,18 +4,22 @@ package javatr.task6.logic;
 import javatr.task6.util.Data6;
 
 public class Logic6 {
-    public static int countOfSecondsToThisMoment() {
+    public static String countOfSecondsToThisMoment() {
+        String s = "***";
         int n = Data6.enterCoundOfSecond();
         int hour, min, sec;
         if (n > 86400) {
             System.out.println("Слишком большое число! Больше секунд в сутках нет! ");
+
+        } else {
+            hour = n / 3600;
+            int n1 = n - hour * 3600;
+            min = n1 / 60;
+            n1 = n1 - min * 60;
+            sec = n1;
+            System.out.println("Идет " + n + " секунда суток. Значит к этому моменту прошло  " + hour + " часов,  " + min + " минут , " + sec + " секунд.");
+            return s;
         }
-        hour = n / 3600;
-        int n1 = n - hour * 3600;
-        min = n1 / 60;
-        n1 = n1 - min * 60;
-        sec = n1;
-        System.out.println("Идет секунда суток:" + n + ".   Значит сейчас часов: " + hour + ", минут: " + min + ", секунд: " + sec);
-        return sec;
+        return s;
     }
 }
--- src/by/java/javatr/task6/util/Data6.java
+++ src/by/java/javatr/task6/util/Data6.java
@@ -4,6 +4,7 @@ import javatr.helper.ScannerHelper;
 
 public class Data6 {
     public static int enterCoundOfSecond() {
+        System.out.println("Введите количество секунд с начала суток: ");
         return ScannerHelper.inputIntFromConsole();
     }
 }
