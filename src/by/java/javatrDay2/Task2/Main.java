f217fddeb3305f01c2ddde279fa75040cbdbc567
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-11-21 21:17:25 +0200

    task2day2

--- src/by/java/javatrDay2/Task2/Main.java
+++ src/by/java/javatrDay2/Task2/Main.java
@@ -1,4 +1,5 @@
-package javatrDay2.Task2;/*
+package javatrDay2.Task2;
+/*
 1.создать класс Хелло, который будет приветствовать любого пользователя при вводе его имени через командную строку
 2.создать приложение, которое отображает в окне консоли аргументы командной строки метода мэйн в обратном порядке
 3.создать приложение, выводящее заданное количество случайных чисел с переходом и без перехода на новую строку
@@ -10,72 +11,79 @@ package javatrDay2.Task2;/*
 7. создать приложение, получающее дату рождения (день, месяц и год) и определяющее, в какой день недели вы родились,
 сколько вам полных лет и поздравляет с днем рождения, если оно сегодня (использовать класс Календарь из пакета джава утил
 8. запустить приложение из командной строки (в текстовом документе подробно описать последовательность выполнения со скриншотами)
-
 */
+
+import javatrDay2.helper.ScannerHelper;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //1. Приветствовать любого пользователя при вводе его имени через командную строку.
+//1. Приветствовать любого пользователя при вводе его имени через командную строку.
+        System.out.println("Введите свое имя: ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
-        System.out.println("Hello " + name);
+        System.out.println("Hello,  " + name + " !");
 
-        //2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+//2. Отобразить в окне консоли аргументы командной строки в обратном порядке.
+
         System.out.print("Введите последовательность цифр: ");
-        int value = scanner.nextInt();
+        int value = ScannerHelper.inputInt();
         int mirror = 0;
         while (value != 0) {
             mirror = mirror * 10 + (value % 10);
             value = value / 10;
         }
-        System.out.print("Введите обратную последовательность цифр: " + mirror);
+        System.out.print(mirror);
 
-        //3. Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
-        int[] a = {1, 2, 3, 4, 5};
+//3.создать приложение, выводящее заданное количество случайных чисел с переходом и без перехода на новую строку
+        Random random = new Random();
+        int size = 5;
+        int maxValueOfArray = 10;
+        int[] a = new int[size];
         for (int i : a) {
-            System.out.println(i);
-            System.out.print(i);
+            a[i] = random.nextInt(maxValueOfArray);
+            System.out.println(a[i] + "  ");
+            System.out.print(a[i] + "  ");
         }
-        // 4. Ввести пароль из командной строки и сравнить его со строкой-образцом.
-        System.out.print("Введите пароль: ");
+//4.создать приложение для ввода пароля из командной строки и сравнения его со строкой-образцом
+
+        System.out.println("Введите пароль: ");
         String password1 = scanner.next();
-        String password2 = "tester1";
+        String password2 = "test";
         if (password1.equals(password2)) {
             System.out.println("Пароль верный");
-        } else {System.out.println("Пароль не верный");}
+        } else {
+            System.out.println("Пароль не верный");
+        }
 
-        //5. Ввести целые числа как аргументы командной строки, подсчитать их суммы (произведения) и вывести результат на консоль.
+ //5. Ввести целые числа как аргументы командной строки, подсчитать их суммы (произведения) и вывести результат на консоль.
 
-        System.out.print("Ввести целые цисла: ");
-        int value1 = scanner.nextInt();
+        System.out.println("Ввести целые цисла: ");
+        int value1 = ScannerHelper.inputInt();
         int sum = 0;
         int com = 1;
         while (value1 != 0) {  //подсчитать сумму чисел
             sum = sum + value1 % 10;
             value1 = value1 / 10;
         }
-        System.out.println("Cумма введённых чисел: " + sum);
-        while (value1 != 0) {   //подсчитать произведения чисел
-        com *= value1 % 10;
-        value1 = value1 / 10;
-    }
-        System.out.println("Произведение введённых чисел: " + com);
+        System.out.println("Сумма введенных чисел: " + sum);
+        System.out.println("Произведение введенных чисел: " + com);
 
-        //6. приложение, выводящее фамилию разработчика и за сколько дней готов выполнить задание.
-        //  необходимо рассчитать дату и время сдачи задания.
-        //  Для получения даты и времени использовать класс Календарь из пакета джава утил.
+//6. приложение, выводящее фамилию разработчика и за сколько дней готов выполнить задание.
+ //  необходимо рассчитать дату и время сдачи задания.
+ //  Для получения даты и времени использовать класс Календарь из пакета джава утил.
         String s = "Andrievich";
         Date d = new Date();
         System.out.println("Получения задания для " + s + " " + d);
 
         Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR,2019);
-        c.set(Calendar.MONTH,Calendar.NOVEMBER);
-        c.set(Calendar.DAY_OF_MONTH,26);
+        c.set(Calendar.YEAR, 2019);
+        c.set(Calendar.MONTH, Calendar.NOVEMBER);
+        c.set(Calendar.DAY_OF_MONTH, 26);
 
         System.out.println("Сдача задания для " + s + " " + c.getTime());
     }
