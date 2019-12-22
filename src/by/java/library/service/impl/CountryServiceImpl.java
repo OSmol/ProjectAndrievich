cce90f400cc7e14d543419159fded30886f75cf3
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-12-22 22:53:51 +0200

    library

---
+++ src/by/java/library/service/impl/CountryServiceImpl.java
@@ -0,0 +1,14 @@
+package library.service.impl;
+
+import library.bean.Country;
+import library.service.CountryService;
+import library.service.exception.ServiceException;
+
+import java.util.List;
+
+public class CountryServiceImpl implements CountryService {
+    @Override
+    public List<Country> getAll() throws ServiceException {
+        return null;
+    }
+}
---
+++ src/by/java/library/service/impl/GenreServiceImpl.java
@@ -0,0 +1,14 @@
+package library.service.impl;
+
+import library.bean.Genre;
+import library.service.GenreService;
+import library.service.exception.ServiceException;
+
+import java.util.List;
+
+public class GenreServiceImpl implements GenreService {
+    @Override
+    public List<Genre> getAll() throws ServiceException {
+        return null;
+    }
+}
