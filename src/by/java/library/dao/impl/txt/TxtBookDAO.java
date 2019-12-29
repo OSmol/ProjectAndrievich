d197357aa2c57994f6199c1367db2fb03f3beb59
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-12-29 19:14:59 +0200

    l

--- src/by/java/library/dao/impl/txt/TxtBookDAO.java
+++ src/by/java/library/dao/impl/txt/TxtBookDAO.java
@@ -11,9 +11,36 @@ public class TxtBookDAO implements BookDAO {
 
 
     @Override
-    public void addBook(Book book) throws DAOException {
+    public boolean addBook(Book book, String filename) throws DAOException {
 
-    }
+            boolean b = false;
+            File file = new File(filename);
+            ObjectOutputStream objectOutputStream = null;
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                if (fileOutputStream != null) {
+                    objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                    objectOutputStream.writeObject(book);
+                    b = true;
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println("file cant be create" + e);
+            } catch (NotSerializableException e) {
+                System.err.println("file don't suggesting serialization" + e);
+            } catch (IOException e) {
+                System.err.println("file cant be create" + e);
+            } finally {
+                try {
+                    if (objectOutputStream != null) {
+                        objectOutputStream.close();
+                    }
+                } catch (IOException e) {
+                    System.err.println("error of closing stream" + e);
+                }
+            }
+            return b;
+        }
+
 
     @Override
     public void deleteBook(long idBook) throws DAOException {
@@ -26,9 +53,35 @@ public class TxtBookDAO implements BookDAO {
     }
 
     @Override
-    public void getBook(int idBook) throws DAOException {
+    public Book getBook(int idBook, String filename) throws DAOException, InvalidObjectException {
+       // public Book deserialization(String filename) throws InvalidObjectException {
+            File file = new File(filename);
+            ObjectInputStream objectInputStream = null;
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                objectInputStream = new ObjectInputStream(fileInputStream);
+                Book book = (Book) objectInputStream.readObject();
+                return book;
+            } catch (ClassNotFoundException ce) {
+                System.err.println("Класс не существует: " + ce);
+            } catch (FileNotFoundException e) {
+                System.err.println("Файл для десериализации не существует: " + e);
+            } catch (InvalidClassException ioe) {
+                System.err.println("Несовпадение версий классов: " + ioe);
+            } catch (IOException ioe) {
+                System.err.println("Общая IO ошибка: " + ioe);
+            } finally {
+                try {
+                    if (objectInputStream != null) {
+                        objectInputStream.close();
+                    }
+                } catch (IOException e) {
+                    System.err.println("ошибка закрытия потока ");
+                }
+            }
+            throw new InvalidObjectException("объект не восстановлен");
+        }
 
-    }
 
     @Override
     public void getAllBooks(Book book) throws DAOException {
@@ -39,60 +92,7 @@ public class TxtBookDAO implements BookDAO {
     public void updateBook(Book book) throws DAOException {
 
     }
-    public boolean serialization(Book book, String filename) {
-        boolean b = false;
-        File file = new File(filename);
-        ObjectOutputStream objectOutputStream = null;
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            if (fileOutputStream != null) {
-                objectOutputStream = new ObjectOutputStream(fileOutputStream);
-                objectOutputStream.writeObject(book);
-                b = true;
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("file cant be create" + e);
-        } catch (NotSerializableException e) {
-            System.err.println("file don't suggesting serialization" + e);
-        } catch (IOException e) {
-            System.err.println("file cant be create" + e);
-        } finally {
-            try {
-                if (objectOutputStream != null) {
-                    objectOutputStream.close();
-                }
-            } catch (IOException e) {
-                System.err.println("error of closing stream" + e);
-            }
-        }
-        return b;
-    }
 
-    public Book deserialization(String filename) throws InvalidObjectException {
-        File file = new File(filename);
-        ObjectInputStream objectInputStream = null;
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            objectInputStream = new ObjectInputStream(fileInputStream);
-            Book book = (Book) objectInputStream.readObject();
-            return book;
-        } catch (ClassNotFoundException ce) {
-            System.err.println("Класс не существует: " + ce);
-        } catch (FileNotFoundException e) {
-            System.err.println("Файл для десериализации не существует: " + e);
-        } catch (InvalidClassException ioe) {
-            System.err.println("Несовпадение версий классов: " + ioe);
-        } catch (IOException ioe) {
-            System.err.println("Общая IO ошибка: " + ioe);
-        } finally {
-            try {
-                if (objectInputStream != null) {
-                    objectInputStream.close();
-                }
-            } catch (IOException e) {
-                System.err.println("ошибка закрытия потока ");
-            }
-        }
-        throw new InvalidObjectException("объект не восстановлен");
-    }
+
+
 }
