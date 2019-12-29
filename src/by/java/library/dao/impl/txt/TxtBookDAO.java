f008df23516b4e9d7334452e85092d7eb6b35c8c
TatsianaRrr <tatsiana.andryievich@gmail.com>
2019-12-29 19:36:16 +0200

    l

--- src/by/java/library/dao/impl/txt/TxtBookDAO.java
+++ src/by/java/library/dao/impl/txt/TxtBookDAO.java
@@ -38,58 +38,60 @@ public class TxtBookDAO implements BookDAO {
                 }
             }
 
-        } return b;}
-
-
-        @Override
-        public void deleteBook ( long idBook) throws DAOException {
-
         }
-
-        @Override
-        public void deleteBook (Book book) throws DAOException {
-
-        }
-
-        @Override
-        public Book getBook ( int idBook) throws DAOException, InvalidObjectException {
-            // public Book deserialization(String filename) throws InvalidObjectException {
-            File file = new File(BOOKFILE);
-            ObjectInputStream objectInputStream = null;
-            try {
-                FileInputStream fileInputStream = new FileInputStream(file);
-                objectInputStream = new ObjectInputStream(fileInputStream);
-                return (Book) objectInputStream.readObject();
-            } catch (ClassNotFoundException ce) {
-                System.err.println("Класс не существует: " + ce);
-            } catch (FileNotFoundException e) {
-                System.err.println("Файл для десериализации не существует: " + e);
-            } catch (InvalidClassException ioe) {
-                System.err.println("Несовпадение версий классов: " + ioe);
-            } catch (IOException ioe) {
-                System.err.println("Общая IO ошибка: " + ioe);
-            } finally {
-                try {
-                    if (objectInputStream != null) {
-                        objectInputStream.close();
-                    }
-                } catch (IOException e) {
-                    System.err.println("ошибка закрытия потока ");
-                }
-            }
-            throw new InvalidObjectException("объект не восстановлен");
-        }
+        return b;
+    }
 
 
-        @Override
-        public void getAllBooks (Book book) throws DAOException {
-
-        }
-
-        @Override
-        public void updateBook (Book book) throws DAOException {
-
-        }
-
+    @Override
+    public void deleteBook(long idBook) throws DAOException {
 
     }
+
+    @Override
+    public void deleteBook(Book book) throws DAOException {
+
+    }
+
+    @Override
+    public Book getBook(int idBook) throws DAOException, InvalidObjectException {
+        // public Book deserialization(String filename) throws InvalidObjectException {
+        File file = new File(BOOKFILE);
+        ObjectInputStream objectInputStream = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            return (Book) objectInputStream.readObject();
+        } catch (ClassNotFoundException ce) {
+            System.err.println("Класс не существует: " + ce);
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл для десериализации не существует: " + e);
+        } catch (InvalidClassException ioe) {
+            System.err.println("Несовпадение версий классов: " + ioe);
+        } catch (IOException ioe) {
+            System.err.println("Общая IO ошибка: " + ioe);
+        } finally {
+            try {
+                if (objectInputStream != null) {
+                    objectInputStream.close();
+                }
+            } catch (IOException e) {
+                System.err.println("ошибка закрытия потока ");
+            }
+        }
+        throw new InvalidObjectException("объект не восстановлен");
+    }
+
+
+    @Override
+    public void getAllBooks(Book book) throws DAOException {
+
+    }
+
+    @Override
+    public void updateBook(Book book) throws DAOException {
+
+    }
+
+
+}
