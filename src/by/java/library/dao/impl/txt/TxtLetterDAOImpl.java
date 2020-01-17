babfb86360256bd31dcd997abac9bcc433210878
TatsianaRrr <tatsiana.andryievich@gmail.com>
2020-01-18 00:10:42 +0200

    l

--- src/by/java/library/dao/impl/txt/TxtLetterDAOImpl.java
+++ src/by/java/library/dao/impl/txt/TxtLetterDAOImpl.java
@@ -1,6 +1,13 @@
 package library.dao.impl.txt;
 
+import library.bean.Letter;
 import library.dao.LetterDAO;
+import library.service.exception.ServiceException;
+
+import java.util.List;
 
 public class TxtLetterDAOImpl implements LetterDAO {
+    public List<Letter> getAll() throws ServiceException {
+        return null;
+    }
 }
