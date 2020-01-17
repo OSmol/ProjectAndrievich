dfd388abaaf912d8e5389ccf7cfc592750e3a60b
TatsianaRrr <tatsiana.andryievich@gmail.com>
2020-01-18 00:10:22 +0200

    l

--- src/by/java/library/service/impl/LetterServiceImpl.java
+++ src/by/java/library/service/impl/LetterServiceImpl.java
@@ -1,6 +1,20 @@
 package library.service.impl;
 
+import library.bean.Email;
+import library.bean.Letter;
+import library.dao.factory.DAOFactory;
 import library.service.LetterService;
+import library.service.exception.ServiceException;
+import org.apache.log4j.Logger;
+
+import java.util.List;
 
 public class LetterServiceImpl implements LetterService {
+    private static Logger logger = Logger.getLogger(LetterServiceImpl.class);
+    private final DAOFactory daoFactory = DAOFactory.getInstance();
+
+    public List<Letter> getAll() throws ServiceException {
+        logger.debug("LetterServiceImpl - run");
+        return daoFactory.getTxtLetterDAO().getAll();
+    }
 }
