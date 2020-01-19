3060dd354f3abb37a77169e6f45f7e59b75c65c4
TatsianaRrr <tatsiana.andryievich@gmail.com>
2020-01-19 10:38:23 +0200

    l

--- src/by/java/library/MainMenu.java
+++ src/by/java/library/MainMenu.java
@@ -16,24 +16,6 @@ public class MainMenu {
     private static Library library = null;
     private static TxtUserDAOImpl txtUserDAO = new TxtUserDAOImpl();
 
-    public static void getMainOptions(User.UserRole userRole) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\nAvailable options:\n").append("1. View books in the catalog.\n");
-        sb.append("2. Find a book in the catalog.\n");
-        if (userRole == User.UserRole.ADMIN) {
-            sb.append("3. Add a book to the catalog.\n");
-            sb.append("4. Remove book from catalog.\n");
-            sb.append("5. Register a new user.\n");
-            sb.append("6. Delete user.\n");
-        } else {
-            sb.append("3. Suggest an administrator to add a book.\n");
-        }
-        sb.append("9. Change user\n");
-        sb.append("0. To finish work.\n");
-        System.out.println(sb.toString());
-
-    }
-
     public static boolean entrance() {
         User user = null;
         while (user == null) {
